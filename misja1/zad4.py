a5f689da79208e97a4c181faf0df141c6f133222
sdrapinski <szymondrapinski@gmail.com>
2024-04-16 22:57:06 +0200

    animation

--- misja1/zad4.py
+++ misja1/zad4.py
@@ -12,17 +12,56 @@ leg = Cube()
 speed_y = 0.0  # Prędkość obrotu wokół osi Y [rad/s]
 speed_x = 0.0  # Prędkość obrotu wokół osi X [rad/s]
 
+# obserwator
+observer_eye= glm.vec3(0.0, 0.0, -5.0)  # Początkowa pozycja obserwatora
+observer_front = glm.vec3(0.0, 0.0, 0.0)  # Wektor kierunku obserwatora
+observer_up = glm.vec3(0.0, 1.0, 0.0)  # Wektor wskazujący "górę" dla obserwatora
+
+animation_speed = 1.0  # Prędkość animacji
+current_animation_speed = 1.0  # Aktualna prędkość animacji
+
+
 def key_callback(window, key, scancode, action, mods):
-	global speed_y, speed_x
+	global speed_y, speed_x, observer_eye, observer_front, observer_up, current_animation_speed
 	if action == glfw.PRESS:
 		if key == glfw.KEY_LEFT:
-			speed_y = -3.14
-		if key == glfw.KEY_RIGHT:
-			speed_y = 3.14
-		if key == glfw.KEY_UP:
-			speed_x = -3.14
-		if key == glfw.KEY_DOWN:
-			speed_x = 3.14
+			observer_front +=  glm.vec3(0.5, 0.0, 0.0)
+		elif key == glfw.KEY_RIGHT:
+			observer_front -=  glm.vec3(0.5, 0.0, 0.0)
+		elif key == glfw.KEY_UP:
+			observer_front +=  glm.vec3(0.0, 0.5, 0.0)
+		elif key == glfw.KEY_DOWN:
+			observer_front -=  glm.vec3(0.0, 0.5, 0.0)
+		elif key == glfw.KEY_W:
+			observer_eye += glm.vec3(0.0, 0.0, 0.5)
+		elif key == glfw.KEY_S:
+			observer_eye -=  glm.vec3(0.0, 0.0, 0.5)
+		elif key == glfw.KEY_A:
+			observer_eye -= glm.vec3(0.5, 0.0, 0.0)
+		elif key == glfw.KEY_D:
+			observer_eye += glm.vec3(0.5, 0.0, 0.0)
+		elif key == glfw.KEY_Z:
+			observer_eye += glm.vec3(0.0, 0.5, 0.0)
+		elif key == glfw.KEY_X:
+			observer_eye -= glm.vec3(0.0, 0.5, 0.0)
+		elif key == glfw.KEY_1:
+			current_animation_speed = 1.0
+		elif key == glfw.KEY_2:
+			current_animation_speed = 2.0
+		elif key == glfw.KEY_3:
+			current_animation_speed = 3.0
+		elif key == glfw.KEY_4:
+			current_animation_speed = 4.0
+		elif key == glfw.KEY_5:
+			current_animation_speed = 5.0
+		elif key == glfw.KEY_6:
+			current_animation_speed = 6.0
+		elif key == glfw.KEY_7:
+			current_animation_speed = 7.0
+		elif key == glfw.KEY_8:
+			current_animation_speed = 8.0
+		elif key == glfw.KEY_9:
+			current_animation_speed = 9.0
 	if action == glfw.RELEASE:
 		if key == glfw.KEY_LEFT or key == glfw.KEY_RIGHT:
 			speed_y = 0.0
@@ -36,12 +75,13 @@ def init_opengl_program(window):
 
 
 def draw_scene(window, angle_x, angle_y):
+	
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
 	V = glm.lookAt(
-		glm.vec3(0.0, 0.0, -5.0),
-		glm.vec3(0.0, 0.0, 0.0),
-		glm.vec3(0.0, 1.0, 0.0)
+		observer_eye,  # Użyj aktualnej pozycji obserwatora
+        observer_front,  # Patrz w kierunku frontu obserwatora
+        observer_up
 	)
 	P = glm.perspective(glm.radians(50.0), 1.0, 1.0, 50.0)
 
@@ -72,7 +112,7 @@ def draw_scene(window, angle_x, angle_y):
 	# Renderowanie nóg stołu
 	leg_scale_x = 0.1  # Skala szerokości nogi
 	leg_scale_z = 0.1  # Skala głębokości nogi
-	leg_scale_y = 0.9  # Skala wysokości nogi
+	leg_scale_y = 1  # Skala wysokości nogi
 	leg_position_y = -2.0 + 0.5 * leg_scale_y  # Wysokość nóg na poziomie blatu stołu
 
 	# Rysowanie czterech nóg stołu w odpowiednich miejscach
@@ -92,6 +132,7 @@ def free_opengl_program(window):
 	pass
 
 def main():
+	global current_animation_speed
 	glfw.init()
 	window = glfw.create_window(500, 500, "Szymon Drapinski Herbata", None, None)
 	glfw.make_context_current(window)
@@ -112,6 +153,7 @@ def main():
 		angle_y += speed_y * time # Aktualizuj kat obrotu wokół osi Y zgodnie z prędkością obrotu
 
 		draw_scene(window, angle_x, angle_y)
+		#glfw.sleep(1.0 / (60 * current_animation_speed))
 		glfw.poll_events()
 
 
